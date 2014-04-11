5f192cd9d5a8bbc80246de85770a4f3a4b4a4eea
S0AndS0 <strangerthanbland@gmail.com>
2014-04-10 18:21:36 -0700

    inporting things now to make things a bit easier to juggle

--- Install_Scripts/OpenStack/Components/Controller/Network_Interfaces_Modder.sh
+++ Install_Scripts/OpenStack/Components/Controller/Network_Interfaces_Modder.sh
@@ -9,18 +9,9 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 osCN_fullScriptPath="$(readlink -f $0)"
 # deleat last componit from ThisScript and store to another variable
 osCN_ScriptDirectory="$(dirname $osCN_fullScriptPath)"
-IP_inet_addr=`/sbin/ifconfig | grep -E 'inet addr' | grep -v '127.0.0.1' | awk '{gsub("addr:",""); print $2}'`
-IP_Bcast_addr=`/sbin/ifconfig | grep -E 'Bcast' | grep -v '127.0.0.1' | awk '{gsub("Bcast:",""); print $3}'`
-IP_Mask_addr=`/sbin/ifconfig | grep -E 'Mask' | grep -v '127.0.0.1' | awk '{gsub("Mask:",""); print $4}'`
-IP_inet6_addr=`/sbin/ifconfig | grep -E 'inet6 addr' | grep -v '127.0.0.1' | grep -v '1/128' | awk '{gsub("addr:",""); print $2}'`
-IP_local=`/sbin/ifconfig | grep -E 'inet addr' | grep -E '127.0.0.1' | awk '{gsub("addr:",""); print $2}'`
-IP_exteral=`wget -q -O - checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'`
-trimIP=`$IP_Bcast_addr | sed 's/...$//'`
-trimRoute=`route -n | grep -E 'UG' | awk '{print $2}'`
-IPmin=1
-IPmax=254
-customPinger=`for ip in $(seq $IPmin $IPmax) ;do (ping -c 1 -w 5 $trimIP$ip >/dev/null && echo "$trimIP$ip" &) ; done`
-fLive_IPs=`$customPinger | grep -v '$trimRoute' | grep -v '$IP_inet_addr'`
+# read variables from ShairedVariables.sh and functions from userVariable_Writer
+source $osComponent_filePath/Components/Controller/ShairedVariables.sh
+source $osComponent_filePath/Components/Controller/userVariable_Writer
 # functions for script
 promptTo_Write () { 
 read -r -p "Are you sure? [Y/n]" response
