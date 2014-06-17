adb34ee4389ae62aadeef3d05c4c5adfceff1dcd
S0AndS0 <strangerthanbland@gmail.com>
2014-06-17 11:34:27 -0700

    due to the recent news of Ghash or cex.io gaining a +51% of hashing power more than once and for a sustained period I've started a new script for quickly setting up p2pool mining nodes for as many coins as I can find. Sugestions on scripting up the security side of things for a public node are most welcome.

---
+++ Install_Scripts/CriptoCurancies/P2Pool_Installer.sh
@@ -0,0 +1,21 @@
+
+
+
+
+# credits
+# wonderful step by step guid on setting up Merged mining and Litecoin mining.
+# http://www.bitcointrading.com/forum/mining-pools/a-complete-guide-to-p2pool-merged-mining-(btcnmcdvcixci0c)-plus-ltc-linux/
+# 	p2pool for vertcoin
+# http://coingolem.com/how-to-setup-your-own-p2pool-node-144/
+# 	p2pool for dogecoin
+# http://doges.org/index.php?topic=5586.0
+# 	p2pool for darkcoin
+# http://www.reddit.com/r/DRKCoin/comments/1zg2c8/tutorial_how_to_set_up_a_darkcoin_p2pool_server/
+#	p2pool for quarkcoin
+# http://forum.qrk.cc/thread/1210/quark-p2pool-setup
+# 	an indepth guide for p2pool bitcoin node
+# http://techie.pro/2014/05/23/how-to-make-your-own-p2pool-bitcoin-node-part-1/
+# http://techie.pro/2014/05/24/how-to-make-your-own-p2pool-bitcoin-node-part-2/
+
+
+
