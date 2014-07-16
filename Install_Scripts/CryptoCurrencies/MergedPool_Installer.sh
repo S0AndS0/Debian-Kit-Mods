1eaa2ed00bbba5786365cb81a38953a6897381be
S0AndS0 <strangerthanbland@gmail.com>
2014-07-15 17:47:16 -0700

    temp fix for root users; was popping odd errors of "no USER set"

--- Install_Scripts/CryptoCurrencies/MergedPool_Installer.sh
+++ Install_Scripts/CryptoCurrencies/MergedPool_Installer.sh
@@ -4,7 +4,8 @@ echo "Please consider donating to the maintainer of this github by following the
 echo "https://github.com/S0AndS0/Debian-Kit-Mods/blob/master/DonationLinks.txt"
 echo "___setting_variables"
 # Variables
- : ${USER?} ${HOME?}
+#  : ${USER?}
+ : ${HOME?}
 varDATE="$(echo $(date))"
 # find the name of this script and store it to a variable
 _fullScriptPath="$(readlink -f $0)"
