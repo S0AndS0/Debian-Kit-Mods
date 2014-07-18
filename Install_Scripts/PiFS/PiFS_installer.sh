92d9a888a7de5c8453ba4720ed27deac117c7a99
S0AndS0 <strangerthanbland@gmail.com>
2014-07-18 14:00:45 -0700

    working on new script for pifs; looks like I'll need to write one for Fuse as well.

---
+++ Install_Scripts/PiFS/PiFS_installer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+PATH=/bin:/usr/bin:/usr/local/bin ; export PATH 
+echo "Please consider donating to the maintainer of this github by following the following link"
+echo "https://github.com/S0AndS0/Debian-Kit-Mods/blob/master/DonationLinks.txt"
+echo "___setting_variables"
+# Variables
+#  : ${USER?}
+ : ${HOME?}
+varDATE="$(echo $(date))"
+# find the name of this script and store it to a variable
+_fullScriptPath="$(readlink -f $0)"
+# delete last component from ThisScript and store to another variable
+_ScriptDirectory="$(dirname $_fullScriptPath)"
+
+
+# Install dependancies
+apt-get install make automake gcc libtool autoconf2.13
+apt-get install libfuse-dev
+
+# clone pifs source files
+git clone https://github.com/philipl/pifs
+
+# change directories and install pifs with make commands
+cd pifs
+./autogen.sh
+./configure 
+make 
+make install
+
+echo exit
+
+# example commands
+# πfs -o mdd=<metadata directory> <mountpoint>
+# where the metadata directory is where πfs should store its metadata (such as filenames or the locations of your files in π) and mountpoint is your usual filesystem mountpoint.
+
